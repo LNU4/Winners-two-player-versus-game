56f6b5c6f97b0a3c67adac545e3194274df1885a
LNU4 <nasser.alobaedi@gmail.com>
2024-04-26 13:12:01 +0200

    Working layers
    - Added the texture of the soldiers, requires impelementation
    - Adjusted the layers in 3 types
    - Located the players in respective container "layer".
    - Fixed the bullets and it's rotation
    - Now bullets go on layer 1 "aka layer 1".
    - Requires further adjuiments for hit tests and such.

--- Winners/src/scene/game/Game.js
+++ Winners/src/scene/game/Game.js
@@ -57,19 +57,27 @@ Winners.scene.Game.prototype.constructor = Winners.scene.Game;
  */
 Winners.scene.Game.prototype.init = function() {
     rune.scene.Scene.prototype.init.call(this);
+
     this.bg = new rune.display.Graphic(0,0,1280,720,"background");
-
     this.layer0 = new rune.display.DisplayObjectContainer(0,0, 1280, 720);
-    console.log(this.layer0.numChildren);
-    this.stage.addChild(this.layer0); 
+    this.layer1 = new rune.display.DisplayObjectContainer(0,0, 1280, 720);
+    this.layer2 = new rune.display.DisplayObjectContainer(0,0, 1280, 720);
     
-    this.player = new Winners.entity.Player(70, 359.5);
-    this.player2 = new Winners.entity.Player2(1150, 359.5, this.stage, this.player);
-
-    //this.player.player2 = this.player2;
+    
+    this.stage.addChild(this.layer0);
+    this.stage.addChild(this.layer1);
+    this.stage.addChild(this.layer2); 
+    
+   
 
     this.turret1 = new Winners.entity.Turret1(70, 359.5);
-    this.turret2 = new Winners.entity.Turret2(1150, 359.5, this.stage, this.player); 
+    this.layer2.addChild(this.turret1);
+
+    this.turret2 = new Winners.entity.Turret2(1150, 359.5, this.layer0, this.player);
+
+    this.player = new Winners.entity.Player(70, 359.5, this.turret1, this.layer1); 
+    this.player2 = new Winners.entity.Player2(1150, 359.5, this.layer0, this.player);
+
 
     this.base = new Winners.entity.Base(10, 359.5);
     this.base2 = new Winners.entity.Base2(1220, 359.5);
@@ -84,13 +92,13 @@ Winners.scene.Game.prototype.init = function() {
     this.layer0.addChild(this.hp2);
     this.layer0.addChild(this.bg);
     this.layer0.addChild(this.player);
-    this.stage.addChild(this.player2);
-    this.stage.addChild(this.base);
-    this.stage.addChild(this.base2);
-    this.stage.addChild(this.turret1);
-    this.stage.addChild(this.turret2);
-    this.stage.addChild(this.Base1shield);
-    this.stage.addChild(this.Base2shield);
+    this.layer0.addChild(this.player2);
+    this.layer0.addChild(this.base);
+    this.layer0.addChild(this.base2);
+    
+    this.layer2.addChild(this.turret2);
+    this.layer0.addChild(this.Base1shield);
+    this.layer0.addChild(this.Base2shield);
     console.log(this.stage.numChildren)
 };
 
