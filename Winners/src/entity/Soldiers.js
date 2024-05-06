fb0d3eaafcece5532d850dd05c06f86f64f8c2c4
LNU4 <nasser.alobaedi@gmail.com>
2024-05-06 02:33:08 +0200

    Unsure if its improvements
    - Potiental fix to the Soldiers standing.
    - We can not remove the usage of atan2, in order to get 360 movements for the soldiers, bullets .. etc.
    - It looks more complex, but I re wrote the code and sorted it out.

--- Winners/src/entity/Soldiers.js
+++ Winners/src/entity/Soldiers.js
@@ -1,141 +1,124 @@
-Winners.entity.Soldiers = function (x, y, targetPlayer, layer, play){
+//------------------------------------------------------------------------------
+// Constructor scope
+//------------------------------------------------------------------------------
+
+/**
+ * Creates a new object.
+ *
+ * @constructor
+ * @extends rune.display.Sprite
+ *
+ * @class
+ * @classdesc
+ *
+ * Game scene.
+ */
+/**
+ * 
+ */
+Winners.entity.Soldiers = function(x, y, targetPlayer, layer, play) {
     this.targetPlayer = targetPlayer;
     this.shootDistance = 200;
-    this.moveSpeed = 2;
+    this.moveSpeed = 1;
     this.shootCooldown = 900;
     this.lastShootTime = 0;
     this.play = play;
-    
     this.layer = layer;
     this.baseOwner = this.targetPlayer.player2Base;
-    this.baseTarget = this.targetPlayer.enemy1Base ;
-    console.log(this.baseOwner)
+    this.baseTarget = this.targetPlayer.enemy1Base;
 
+    
     rune.display.Sprite.call(this, x, y, 32, 32, "soldiers");
     this.layer.addChild(this);
-
 };
+
+//----------------------------------------------------------------------------
+// Inheritance
+//------------------------------------------------------------------------------
 
 Winners.entity.Soldiers.prototype = Object.create(rune.display.Sprite.prototype);
 Winners.entity.Soldiers.prototype.constructor = Winners.entity.Soldiers;
+//------------------------------------------------------------------------------
+// Override public prototype methods (ENGINE)
+//------------------------------------------------------------------------------
 
 
-Winners.entity.Soldiers.prototype.update = function (step) {
+Winners.entity.Soldiers.prototype.update = function(step) {
     rune.display.Sprite.prototype.update.call(this, step);
 
-    var dx = this.targetPlayer.x - this.x;
-    var dy = this.targetPlayer.y - this.y;
-    var distance = Math.sqrt(dx * dx + dy * dy);
- //console.log(this.play)
-    if (this.play.player2.hitTest(this))   {
-        console.log('HIT')
-        this.play.layer0.removeChild(this);
-    } 
-    if (distance <= this.shootDistance && distance > 90) {
-        
-        this.x = this.x;
-        this.y = this.y;
+   
+    var currentPosition = new rune.geom.Point(this.x, this.y);
+    var targetPosition = new rune.geom.Point(this.targetPlayer.center.x, this.targetPlayer.center.y);
+    var distanceX = targetPosition.x - currentPosition.x;
+    var distanceY = targetPosition.y - currentPosition.y;
+    var distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
 
-        var currentTime = Date.now();
-        if (currentTime - this.lastShootTime >= this.shootCooldown) {
-            this.shoot();
-            this.lastShootTime = currentTime;
-        }
-    } else {
-        
-        dx /= distance;
-        dy /= distance;
-        this.x += dx * this.moveSpeed;
-        this.y += dy * this.moveSpeed;
-    }
+
+     // in order to achieve what we want, we need to rotate the bullet, we need to know the angle between the bullet/player and the target
+     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/atan2
+    var angle = Math.atan2(distanceY, distanceX);
+    
+    var rotationCords = angle * (180 / Math.PI);
+   
+    this.rotation = rotationCords;
+    
+    var directionX = distanceX / distance;
+    var directionY = distanceY / distance;
+
+   if (this.shootDistance <= distance ) {
+    currentPosition.x += directionX * this.moveSpeed;
+    currentPosition.y += directionY * this.moveSpeed;
+   }
+    currentPosition.x = Math.min(Math.max(currentPosition.x, 0), 1280 - this.width);
+    currentPosition.y = Math.min(Math.max(currentPosition.y, 0), 720 - this.height);
 
     
-    var minX = 0;
-    var minY = 0;
-    var maxX = 1280 - this.width;
-    var maxY = 720 - this.height;
-    this.x = Math.min(Math.max(this.x, minX), maxX);
-    this.y = Math.min(Math.max(this.y, minY), maxY);
-};
+    this.x = currentPosition.x;
+    this.y = currentPosition.y;
 
-
-Winners.entity.Soldiers.prototype.shoot = function () {
     var currentTime = Date.now();
-
     if (currentTime - this.lastShootTime >= this.shootCooldown) {
+        this.shoot();
+        this.lastShootTime = currentTime;
+    }
 
-        var startX = this.centerX;
-        var startY = this.centerY;
-
-        var dx = this.targetPlayer.centerX - startX;
-        var dy = this.targetPlayer.centerY - startY;
-        var distance = Math.sqrt(dx * dx + dy * dy);
-
-
-        if (distance <= this.shootDistance) {
-
-            var vx = dx / distance;
-            var vy = dy / distance;
-
-
-            if (!this.bullets) {
-                this.bullets = new Winners.entity.Bullets(this.layer, this, this.turret1, this.targetPlayer, this.baseOwner, this.baseTarget);
-                this.application.scenes.selected.groups.add(this.bullets);
-            }
-
-            var bullet = this.bullets.create(startX, startY);
-
-
-            var bulletSpeed = 1;
-            bullet.velocity.x = vx * bulletSpeed;
-            bullet.velocity.y = vy * bulletSpeed;
-
-            var rotationRadians = Math.atan2(dy, dx);
-            bullet.rotation = rotationRadians * (180 / Math.PI);
-
-
-            this.lastShootTime = currentTime;
-
-            /* old code, perhaps better but requires further checks
-               /*
-    // Calculate start position and velocity
-    var startX = this.x;
-    var startY = this.y;
-    var dx = this.targetPlayer.x - startX;
-    var dy = this.targetPlayer.y - startY;
-    var distance = Math.sqrt(dx * dx + dy * dy);
-    
-    if (distance > 0) {
-        var bulletSpeed = 5;
-        var vx = (dx / distance) * bulletSpeed;
-        var vy = (dy / distance) * bulletSpeed;
-        
-        
-        var bullet = new Winners.entity.Bullet(this.container, this, this.targetPlayer, startX, startY);
-        
-        
-        bullet.velocity.x = vx;
-        bullet.velocity.y = vy;
-        
-       
-        this.parent.addChild(bullet);
-    } 
-    
-    
-            
-            this.bullets = new Winners.entity.Bullets(this.layer, this, this.turret1, this.targetPlayer);
-            this.application.scenes.selected.groups.add(this.bullets);
-            this.bullet = this.bullets.create(this.centerX, this.centerY);
-    
-    
-            this.bullet.velocity.x = this.velocity.x;
-            this.bullet.velocity.y = this.velocity.y;
-            this.bullet.globalX = this.velocity.x;
-             this.bullet.globalX = this.velocity.x;
-    
-             this.bullet.rotation = this.newPoint;
-    
-            */
-        }
+  
+    if (this.play.player2.hitTest(this)) {
+        console.log('HIT');
+        this.play.layer0.removeChild(this);
     }
 };
+
+
+
+Winners.entity.Soldiers.prototype.shoot = function() {
+    var currentPosition = new rune.geom.Point(this.centerX, this.centerY);
+    var targetPosition = new rune.geom.Point(this.targetPlayer.centerX, this.targetPlayer.centerY);
+    
+    
+    var distanceX = targetPosition.x - currentPosition.x;
+    var distanceY = targetPosition.y - currentPosition.y;
+    
+    
+    var distance = currentPosition.distance(targetPosition);
+    console.log(distance);
+    if (distance <= this.shootDistance) {
+        var bulletSpeed = 1;
+        
+        
+        var bulletDirectionX = distanceX / distance;
+        var bulletDirectionY = distanceY / distance;
+
+            this.bullets = new Winners.entity.Bullets(this.layer, this, this.turret1, this.targetPlayer, this.baseOwner, this.baseTarget);
+            this.application.scenes.selected.groups.add(this.bullets);
+        
+        var bullet = this.bullets.create(this.centerX, this.centerY);
+        bullet.velocity.x = bulletDirectionX * bulletSpeed;
+        bullet.velocity.y = bulletDirectionY * bulletSpeed;
+
+     
+        bullet.rotation = Math.atan2(distanceY, distanceX) * (180 / Math.PI);
+    }
+};
+
+
