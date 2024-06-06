65822660719d77041b379dbc6e4cb26e6fc192f0
LNU4 <112617909+LNU4@users.noreply.github.com>
2024-06-06 15:15:54 +0200

    readjusted textures for the new soldier logic
    - Deleted code that was unused
    - Adjusted all the soldier classes textures to illustrate similar frame sequence "not the best, but the best possible"

--- Winners/src/entity/Soldier/Soldier.js
+++ Winners/src/entity/Soldier/Soldier.js
@@ -17,7 +17,7 @@ Winners.entity.Soldier = function (x, y, game, enemy, i, SoldierOwner, truck) {
 };
 
 Winners.entity.Soldier.prototype = Object.create(Winners.entity.SoldierUtil.prototype);
-Winners.entity.Soldier.prototype.constructor = Winners.entity.Soldiers;
+Winners.entity.Soldier.prototype.constructor = Winners.entity.Soldier;
 
 
 /*
@@ -46,9 +46,7 @@ Winners.entity.Soldier.prototype.shoot = function () {
     var bulletDirectionX = this.distanceX / this.distance;
     var bulletDirectionY = this.distanceY / this.distance;
 
-    if (this.animation) {
-      this.animation.gotoAndPlay("shoot");
-    }
+   
 
     var bullet = this.game.bullets.create(this.centerX, this.centerY, this, this.turret1, this.enemy);
 
--- Winners/src/entity/Soldier/SoldierUtil.js
+++ Winners/src/entity/Soldier/SoldierUtil.js
@@ -1,4 +1,3 @@
-
 //------------------------------------------------------------------------------
 // Constructor scope
 //------------------------------------------------------------------------------
@@ -12,45 +11,40 @@
  * @classdesc Game scene.
  */
 Winners.entity.SoldierUtil = function (
-    x,
-    y,
-    game,
-    enemy,
-    spriteType,
-    shootDistance,
-    moveSpeed,
-    shootCooldown,
-    SoldierOwner
+  x,
+  y,
+  game,
+  enemy,
+  spriteType,
+  shootDistance,
+  moveSpeed,
+  shootCooldown,
+  SoldierOwner
 ) {
-    this.shootDistance = shootDistance;
-    this.moveSpeed = moveSpeed;
-    this.shootCooldown = shootCooldown;
-    this.lastShootTime = 0;
-    this.game = game;
-    this.SoldierOwner = SoldierOwner;
-    this.enemy = enemy;
-    this.isDead = false;
-    this.layer = this.game.layer0;
+  this.shootDistance = shootDistance;
+  this.moveSpeed = moveSpeed;
+  this.shootCooldown = shootCooldown;
+  this.lastShootTime = 0;
+  this.game = game;
+  this.SoldierOwner = SoldierOwner;
+  this.enemy = enemy;
+  this.isDead = false;
+  this.layer = this.game.layer0;
 
-    rune.display.Sprite.call(this, x, y, 32, 32, spriteType);
-    this.layer.addChild(this);
+  rune.display.Sprite.call(this, x, y, 32, 32, spriteType);
+  this.layer.addChild(this);
 
-    if (enemy === this.game.player) {
-        this.texture.replaceColor(
-            new rune.color.Color24(102, 102, 102),
-            new rune.color.Color24(172, 50, 50)
-        );
-    } else if (enemy === this.game.player2) {
-        this.texture.replaceColor(
-            new rune.color.Color24(0, 0, 0),
-            new rune.color.Color24(172, 50, 50)
-        );
-    }
-};
-
-Winners.entity.SoldierUtil.prototype.init = function () {
-    rune.display.Sprite.prototype.init.call(this);
-    this.m_initAnimation();
+  if (enemy === this.game.player) {
+    this.texture.replaceColor(
+      new rune.color.Color24(102, 102, 102),
+      new rune.color.Color24(172, 50, 50)
+    );
+  } else if (enemy === this.game.player2) {
+    this.texture.replaceColor(
+      new rune.color.Color24(0, 0, 0),
+      new rune.color.Color24(172, 50, 50)
+    );
+  }
 };
 
 //------------------------------------------------------------------------------
@@ -58,8 +52,17 @@ Winners.entity.SoldierUtil.prototype.init = function () {
 //------------------------------------------------------------------------------
 Winners.entity.SoldierUtil.prototype = Object.create(
     rune.display.Sprite.prototype
-);
-Winners.entity.SoldierUtil.prototype.constructor = Winners.entity.SoldierUtil;
+  );
+  Winners.entity.SoldierUtil.prototype.constructor = Winners.entity.SoldierUtil;
+
+
+Winners.entity.SoldierUtil.prototype.init = function () {
+  rune.display.Sprite.prototype.init.call(this);
+  this.m_initAnimation();
+  
+};
+
+
 
 //------------------------------------------------------------------------------
 // Override public prototype methods (ENGINE)
@@ -70,59 +73,61 @@ Winners.entity.SoldierUtil.prototype.constructor = Winners.entity.SoldierUtil;
  * @returns {undefined}
  */
 Winners.entity.SoldierUtil.prototype.update = function (step) {
-    rune.display.Sprite.prototype.update.call(this, step);
+  rune.display.Sprite.prototype.update.call(this, step);
 
-    this.currentPosition = new rune.geom.Point(this.x, this.y);
-    this.targetPosition = new rune.geom.Point(this.enemy.centerX, this.enemy.centerY);
+  this.currentPosition = new rune.geom.Point(this.x, this.y);
+  this.targetPosition = new rune.geom.Point(
+    this.enemy.centerX,
+    this.enemy.centerY
+  );
 
-    this.distanceX = this.targetPosition.x - this.currentPosition.x;
-    this.distanceY = this.targetPosition.y - this.currentPosition.y;
-    this.distance = this.currentPosition.distance(this.targetPosition);
+  this.distanceX = this.targetPosition.x - this.currentPosition.x;
+  this.distanceY = this.targetPosition.y - this.currentPosition.y;
+  this.distance = this.currentPosition.distance(this.targetPosition);
 
-    if (this.distance <= this.shootDistance && this.distance > 90) {
+  if (this.distance <= this.shootDistance && this.distance > 90) {
+   
+
+    var currentTime = Date.now();
+    if (currentTime - this.lastShootTime >= this.shootCooldown) {
         if (this.animation) {
-            this.animation.gotoAndPlay("idle");
-        }
-
-        var currentTime = Date.now();
-        if (currentTime - this.lastShootTime >= this.shootCooldown) {
-            this.shoot();
-            this.lastShootTime = currentTime;
-        }
-    } else {
-        this.distanceX /= this.distance;
-        this.distanceY /= this.distance;
-        this.x += this.distanceX * this.moveSpeed;
-        this.y += this.distanceY * this.moveSpeed;
-        if (this.animation) {
-            this.animation.gotoAndPlay("walk");
-        }
+            this.animation.gotoAndPlay("shoot");
+          }
+      this.shoot();
+      this.lastShootTime = currentTime;
     }
+  } else {
+    this.distanceX /= this.distance;
+    this.distanceY /= this.distance;
+    this.x += this.distanceX * this.moveSpeed;
+    this.y += this.distanceY * this.moveSpeed;
+    if (this.animation) {
+      this.animation.gotoAndPlay("walk");
+    }
+  }
 
-    this.x = rune.util.Math.clamp(this.x, 0, 1280 - this.width);
-    this.y = rune.util.Math.clamp(this.y, 0, 720 - this.height);
+  this.x = rune.util.Math.clamp(this.x, 0, 1280 - this.width);
+  this.y = rune.util.Math.clamp(this.y, 0, 720 - this.height);
 
-    var angle = Math.atan2(this.distanceY, this.distanceX);
-    this.rotation = angle * (180 / Math.PI);
+  var angle = Math.atan2(this.distanceY, this.distanceX);
+  this.rotation = angle * (180 / Math.PI);
 
-    if (this.enemy.hitTest(this)) {
+  if (this.enemy.hitTest(this)) {
+    this.handleKillSoldier();
+  }
+
+  this.game.bullets.hitTest(
+    this,
+    function (bullet, soldier) {
+      if (bullet.bulletTarget == soldier.SoldierOwner) {
+        console.log(soldier, this);
+        console.log(bullet);
+        this.game.bullets.removeMember(bullet, true);
         this.handleKillSoldier();
-    }
-    
-    this.game.bullets.hitTest(
-        this,
-        function (bullet, soldier) {
-            
-            if (bullet.bulletTarget == soldier.SoldierOwner) {
-                console.log(soldier, this)
-                console.log(bullet)
-                this.game.bullets.removeMember(bullet, true);
-                this.handleKillSoldier();
-            }
-        },
-        this
-    );
-  
+      }
+    },
+    this
+  );
 };
 
 /**
@@ -130,9 +135,10 @@ Winners.entity.SoldierUtil.prototype.update = function (step) {
  * @returns {undefined}
  */
 Winners.entity.SoldierUtil.prototype.m_initAnimation = function () {
-    this.animation.create("shoot", [0, 3], 5, true);
-    this.animation.create("idle", [0], 1, true);
-    this.animation.create("walk", [0, 1], 5, true);
+
+  this.animation.create("shoot", [0, 3, 4, 0], 4, true);
+  this.animation.create("idle", [0], 1, true);
+  this.animation.create("walk", [0, 1], 5, true);
 };
 
 /**
@@ -140,7 +146,7 @@ Winners.entity.SoldierUtil.prototype.m_initAnimation = function () {
  * @returns {undefined}
  */
 Winners.entity.SoldierUtil.prototype.shoot = function () {
-    // To be implemented by specific soldier types
+  // To be implemented by specific soldier types
 };
 
 /**
@@ -148,20 +154,19 @@ Winners.entity.SoldierUtil.prototype.shoot = function () {
  * @returns {undefined}
  */
 Winners.entity.SoldierUtil.prototype.handleKillSoldier = function () {
-    console.log(this)
-    var m_this = this;
-    this.game.layer0.removeChild(this, true);
-    this.isDead = true;
-    var powerUpProb = Math.floor(Math.random() * 4);
+  var m_this = this;
+  this.game.layer0.removeChild(this, true);
+  this.isDead = true;
+  var powerUpProb = Math.floor(Math.random() * 4);
 
-    if ((this.isDead && powerUpProb == 0) || powerUpProb == 2) {
-        this.game.timers.create({
-            duration: 1000,
-            onComplete: function () {
-                m_this.createPowerups();
-            },
-        });
-    }
+  if ((this.isDead && powerUpProb == 0) || powerUpProb == 2) {
+    this.game.timers.create({
+      duration: 1000,
+      onComplete: function () {
+        m_this.createPowerups();
+      },
+    });
+  }
 };
 
 /**
@@ -169,10 +174,10 @@ Winners.entity.SoldierUtil.prototype.handleKillSoldier = function () {
  * @returns {undefined}
  */
 Winners.entity.SoldierUtil.prototype.createPowerups = function () {
-    var ranX = Math.floor(Math.random() * (1160 - 200 + 1)) + 200;
-    var ranY = Math.floor(Math.random() * (600 - 200 + 1)) + 200;
-    var powerUp = new Winners.entity.Powerup(ranX, ranY, this.game, this.enemy);
-    this.game.camera.addChild(powerUp);
+  var ranX = Math.floor(Math.random() * (1160 - 200 + 1)) + 200;
+  var ranY = Math.floor(Math.random() * (600 - 200 + 1)) + 200;
+  var powerUp = new Winners.entity.Powerup(ranX, ranY, this.game, this.enemy);
+  this.game.camera.addChild(powerUp);
 };
 
 /**
@@ -180,5 +185,5 @@ Winners.entity.SoldierUtil.prototype.createPowerups = function () {
  * @returns {undefined}
  */
 Winners.entity.SoldierUtil.prototype.dispose = function () {
-    rune.display.Sprite.prototype.dispose.call(this);
+  rune.display.Sprite.prototype.dispose.call(this);
 };
